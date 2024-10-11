8594f17d633965edea3b9d7631e6c7ac870605cc
Hector Gallego <gallegohector96@gmail.com>
2024-10-11 05:13:43 -0500

    se agragan test faltantentes

--- src/app/core/services/brand/brand.service.spec.ts
+++ src/app/core/services/brand/brand.service.spec.ts
@@ -1,16 +1,55 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BrandService } from './brand.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Brand } from '../../models/brand';
+import { environment } from 'src/environments/environment';
+import { BrandValuesConstants } from 'src/app/shared/constants/brandConstants';
 
-xdescribe('BrandService', () => {
+describe('BrandService', () => {
   let service: BrandService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BrandService],
+    });
     service = TestBed.inject(BrandService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+  it('debería crearse el servicio', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('debería enviar una solicitud POST para añadir una marca', () => {
+    const mockCategoryData: Brand = {
+      name: 'Sony',
+      description: 'Marca de tecnología',
+    };
+    const mockResponse = {
+      success: true,
+      message: 'Marca añadida correctamente',
+    };
+
+    service.addBrand(mockCategoryData).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(
+      environment.stockApiUrl + BrandValuesConstants.END_POINT_BRAND
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.headers.get('Authorization')).toBe(
+      environment.mockTokenAdmin
+    );
+    req.flush(mockResponse);
   });
 });
--- src/app/features/pages/add-brand-page/add-brand-page.component.spec.ts
+++ src/app/features/pages/add-brand-page/add-brand-page.component.spec.ts
@@ -1,23 +1,146 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddBrandPageComponent } from './add-brand-page.component';
+import { BrandService } from 'src/app/core/services/brand/brand.service';
+import { LoaderService } from 'src/app/shared/services/loader/loader.service';
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
+import { Brand } from 'src/app/core/models/brand';
+import { ApiResponse } from 'src/app/core/models/apiResponse';
+import { BrandFormComponent } from 'src/app/components/organism/brand-form/brand-form.component';
+import { InputComponent } from 'src/app/components/atoms/input/input.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-xdescribe('AddBrandPageComponent', () => {
+import {
+  ErrorMessages,
+  StatesTypes,
+} from 'src/app/shared/constants/commonConstants';
+import { of, throwError } from 'rxjs';
+
+describe('AddBrandPageComponent', () => {
   let component: AddBrandPageComponent;
   let fixture: ComponentFixture<AddBrandPageComponent>;
 
+  let brandService: BrandService;
+  let loaderService: LoaderService;
+  let toastService: ToastService;
+
+  const setupMocks = () => {
+    const brandServiceMock = {
+      addBrand: jest.fn(),
+    };
+
+    const loaderServiceMock = {
+      show: jest.fn(),
+      hide: jest.fn(),
+    };
+
+    const toastServiceMock = {
+      showToast: jest.fn(),
+    };
+
+    return { brandServiceMock, loaderServiceMock, toastServiceMock };
+  };
+
+  const formData: Brand = {
+    name: 'Nueva Marca',
+    description: 'descripción',
+  };
+
+  const mockResponse: ApiResponse = {
+    message: 'Marca agregada exitosamente!',
+    status: 200,
+    timestamp: '2024-12-12',
+  };
+
   beforeEach(async () => {
+    const { brandServiceMock, loaderServiceMock, toastServiceMock } =
+      setupMocks();
     await TestBed.configureTestingModule({
-      declarations: [ AddBrandPageComponent ]
-    })
-    .compileComponents();
+      declarations: [AddBrandPageComponent, BrandFormComponent, InputComponent],
+      providers: [
+        { provide: BrandService, useValue: brandServiceMock },
+        { provide: LoaderService, useValue: loaderServiceMock },
+        { provide: ToastService, useValue: toastServiceMock },
+      ],
+      imports: [ReactiveFormsModule, FormsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddBrandPageComponent);
     component = fixture.componentInstance;
+
+    brandService = TestBed.inject(BrandService);
+    loaderService = TestBed.inject(LoaderService);
+    toastService = TestBed.inject(ToastService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  const testSuccessResponse = () => {
+    jest.spyOn(brandService, 'addBrand').mockReturnValue(of(mockResponse));
+    const toastShowSpy = jest.spyOn(toastService, 'showToast');
+    const resetFormSpy = jest.spyOn(component.brandForm, 'resetForm');
+
+    component.onFormSubmit(formData);
+
+    expect(loaderService.show).toHaveBeenCalled();
+    expect(brandService.addBrand).toHaveBeenCalledWith(formData);
+    expect(toastShowSpy).toHaveBeenCalledWith({
+      message: mockResponse.message,
+      duration: 10000,
+      type: StatesTypes.SUCCESS,
+    });
+    expect(resetFormSpy).toHaveBeenCalled();
+    expect(loaderService.hide).toHaveBeenCalled();
+  };
+
+  const testErrorResponse = (error: any) => {
+    jest
+      .spyOn(brandService, 'addBrand')
+      .mockReturnValue(throwError(() => error));
+    const toastShowSpy = jest.spyOn(toastService, 'showToast');
+
+    component.onFormSubmit(formData);
+
+    expect(loaderService.show).toHaveBeenCalled();
+    expect(brandService.addBrand).toHaveBeenCalledWith(formData);
+    expect(toastShowSpy).toHaveBeenCalledWith({
+      message: error.error?.message || ErrorMessages.GENERIC_ERROR_MESSAGE,
+      duration: 10000,
+      type: StatesTypes.ERROR,
+    });
+    expect(loaderService.hide).toHaveBeenCalled();
+  };
+
+  it('debería crear el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería llamar a addBrand y mostrar el toast de éxito', () => {
+    testSuccessResponse();
+  });
+
+  it('debería manejar un error al añadir una marca y mostrar el toast de error', () => {
+    const mockError = { message: ErrorMessages.GENERIC_ERROR_MESSAGE };
+    testErrorResponse(mockError);
+  });
+
+  it('debería manejar un error específico al agregar marca', () => {
+    const mockError = {
+      error: { message: ErrorMessages.GENERIC_ERROR_MESSAGE },
+    };
+    testErrorResponse(mockError);
+  });
+
+  it('debería manejar un error sin mensaje y mostrar el toast de error genérico', () => {
+    const mockError = {};
+    testErrorResponse(mockError);
+  });
+
+  it('debería llamar a hide de loaderService al completar la solicitud', () => {
+    jest.spyOn(brandService, 'addBrand').mockReturnValue(of(mockResponse));
+
+    component.onFormSubmit(formData);
+    expect(loaderService.show).toHaveBeenCalled();
   });
 });
